0353fa4a8438aa0fdb8f4a78bc55afcd25111f87
BrakovIgor19 <XapTMaH19@yandex.ru>
2021-09-04 11:32:08 +0400

    Добавлены структуры

---
+++ Laboratoranya_1/Laboratoranya_1/Laboratoranya_1.cpp
@@ -0,0 +1,29 @@
+﻿// Лабораторная работа № 1
+#include <iostream>
+
+using namespace std;
+
+// Труба
+struct Pipe
+{
+    int id; // Идентификатор трубы
+    double lenght; // Длина трубы
+    double diameter; // Диаметр трубы
+    bool signRepair; // Признак "в ремонте"
+};
+
+// Компрессорная станция
+struct CompressorStation
+{
+    int id; // Идентификатор трубы
+    string name; // Название станции
+    int numberWorkshops; // Количество цехов в станции
+    int effectiveness; // Эффективность станции
+};
+
+int main()
+{
+
+    return 0;
+}
+
